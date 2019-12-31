ee6caad7906dfeba164ab594c76ebb310581cae9
Micah Andres <mandres1@uw.edu>
2019-12-30 14:06:01 -1000

    implemented <ul> tags for site

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 	// Close connection:
 	mysqli_close($conn);
 
-	// print_r($pizzas);
+	// explode(',', $pizzas[0]['ingredients']);
 
 ?>
 
@@ -44,7 +44,11 @@
 					<div class="card z-depth-0">
 						<div class="card-content center">
 							<h6><?php echo htmlspecialchars($pizza['title']) ?></h6>
-							<div><?php echo htmlspecialchars($pizza['ingredients']) ?></div>
+							<ul>
+								<?php foreach(explode(',', $pizza['ingredients']) as $ing) { ?>
+									<li><?php echo  htmlspecialchars($ing)?></li>
+								<?php } ?>
+							</ul>
 						</div>
 						<div class="card-action right-align">
 							<a href="#" class="brand-text">More Information</a>
